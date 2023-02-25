78bfc3b282a8ad67433c8fc5dd16d9f7b29c114f
MuhammadMillwala <106543900+MuhammadMillwala@users.noreply.github.com>
2023-02-25 12:05:39 +0500

    Update calculate.py

--- projects/Calculate_age/calculate.py
+++ projects/Calculate_age/calculate.py
@@ -6,7 +6,7 @@ def judge_leap_year(year1):
     """Judge Leap year"""
     return bool(isleap(year1))
 
-"""returns the number of days in each month """
+"""Judge day of month"""
 def month_days(month, leap_year):
     if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
